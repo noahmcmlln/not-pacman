8e28a637137fa8cd22cbd8d1335716828f6118be
Noah McMullen <noahmcmlln@gmail.com>
2016-03-10 15:44:25 -0500

    Work on making BouncyBall disappear

--- NotPacman.js
+++ NotPacman.js
@@ -5,6 +5,19 @@
 */
 
 // constructor function
+var ballPit = [];
+
+var ballAmount = 20;
+
+var ballStart = function(ball){
+  ball.initialize();
+};
+
+var updateanddisplay = function(ball){
+  ball.update();
+  ball.display();
+};
+
 var NotPacmanGame = function () {
 
 };
@@ -12,15 +25,20 @@ var NotPacmanGame = function () {
 NotPacmanGame.prototype = {
 
   initialize: function () {
+    createCanvas(800, 800);
+    while (ballPit.length < ballAmount) ballPit.push(new BouncyBall(width/2, height/2));
+    ballPit.forEach(ballStart);
 
   },
 
   update: function () {
-
+    for (var index = 0; index < ballPit.length, ++index) {
+      if
+    }
   },
 
   display: function () {
-    
+
   }
 
 };
