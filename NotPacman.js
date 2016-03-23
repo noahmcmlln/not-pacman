ef6e5ee67dc3ccc0e8eea02f87c5a5adf94a0d88
Noah McMullen <noahmcmlln@gmail.com>
2016-03-23 02:50:23 -0400

    Continue formatting code

--- NotPacman.js
+++ NotPacman.js
@@ -5,33 +5,29 @@
 */
 
 // constructor function
-var timer = new Timer();
-timer.pause();
-
-var score = function () {
-  number: 0,
-  scoreDisplay = function () {
-    textSize(40);
-    text(this.number, 560, 20);
-  },
-  scorePlus = function () {
-    score.number = score.number + 1;
-  },
-};
-
 var ballPit = [];
 
 var ballAmount = 20;
 
-var ballStart = function(ball, index, array){
-  ball.initialize();
-};
+this.timer = new Timer();
+timer.pause();
 
-var updateanddisplay = function(ball, index, array){
-  ball.update();
-  ball.display();
-};
+var score = {
+  number: 0,
 
+  display: function () {
+    textSize(40);
+    fill(255);
+    text(this.number, 20, 20);
+  },
+
+  update: function () {
+  },
+
+  scorePlus: function () {
+    this.number++;
+  },
+};
 
 var NotPacmanGame = function () {
 };
@@ -42,8 +38,6 @@ var NotPacmanGame = function () {
       createCanvas(600, 600);
       while (ballPit.length < ballAmount) ballPit.push(new BouncyBall(width/2, height/2));
       ballPit.forEach(ballStart);
-      this.keyedUpBall = new KeyedUpBall;
-      keyedUpBall.initialize();
       this.gameIsThere = 0;
     },
 
@@ -56,16 +50,11 @@ var NotPacmanGame = function () {
     display: function () {
       this.gameSetUp();
       if (this.gameIsThere) this.gameGo();
-
     },
 
-    checkForSplice: function(ball, ballAmount, array) {
-      if (this.keyedUpBall.detectCollision(ball)) this.spliceBalls(ballAmount)
-    },
-
-    spliceBalls : function (ballAmount) {
-      ballPit.splice(ballAmount, 1);
-      this.score.scorePlus;
+    var updateanddisplay = function(otherBall, index, array){
+      otherBall.update();
+      otherBall.display();
     },
 
     gameSetUp: function() {
@@ -81,10 +70,23 @@ var NotPacmanGame = function () {
       ballPit.forEach(this.updateanddisplay);
     },
 
-    beginGame: function() {
-      this.gameIsThere = 1;
+    beginGame: function(x, y) {
       timer.unpause();
-      this.keyedUpBall = new KeyedUpBall(mouseX, mouseY);
+      this.gameIsThere = 1;
+      this.keyedUpBall = new KeyedUpBall(x, y);
       this.keyedUpBall.initialize;
+    },
+
+    var ballStart = function(otherBall, index, array){
+      otherBall.initialize();
+    },
+
+    checkForSplice: function(otherBall, ballAmount, array) {
+      if (this.keyedUpBall.detectCollision(otherBall)) this.spliceBalls(ballAmount)
+    },
+
+    spliceBalls : function (ballAmount) {
+      ballPit.splice(ballAmount, 1);
+      this.score.scorePlus;
     },
   };
